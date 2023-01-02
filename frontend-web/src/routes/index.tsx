cebc848d2f6cb08362da6987219977a7714d7152
Pierre Oliveira <pierre.torres445@gmail.com>
2023-01-02 11:42:59 -0300

    feat: configured routes

---
+++ frontend-web/src/routes/index.tsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from '../pages/Home';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Home />
+	}
+]);
+
+function Routes() {
+	return(
+		<RouterProvider router={router} />
+	);
+}
+
+export default Routes;
