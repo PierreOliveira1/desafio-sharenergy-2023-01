76cbcb99c75641e9012dde74b5a05e72e6aeb5cb
Pierre Oliveira <pierre.torres445@gmail.com>
2023-01-06 16:52:04 -0300

    feat: configured database

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,70 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import Routes from './routes';
+import prisma from './database';
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/', Routes);
+prisma
+	.$connect()
+	.then(async () => {
+		app.use(bodyParser.json());
+		app.use(bodyParser.urlencoded({ extended: false }));
+		app.use('/', Routes);
+		// await prisma.customer.create({
+		// 	data: {
+		// 		email: 'b',
+		// 		cpf: 'b',
+		// 		full_name: 'c',
+		// 		phone_number: 'b',
+		// 		Address: {
+		// 			create: {
+		// 				city: 'b',
+		// 				complement: 'b',
+		// 				country: 'b',
+		// 				district: 'b',
+		// 				number: 1,
+		// 				state: 'b',
+		// 				street: 'b',
+		// 				zip_code: 'b',
+		// 			},
+		// 		},
+		// 	},
+		// });
+
+		// await prisma.customer.update({
+		// 	where: { email: 'a' },
+		// 	data: {
+		// 		Address: {
+		// 			create: {
+		// 				city: 'c',
+		// 				complement: 'c',
+		// 				country: 'c',
+		// 				district: 'c',
+		// 				number: 1,
+		// 				state: 'c',
+		// 				street: 'c',
+		// 				zip_code: 'c',
+		// 			},
+		// 		},
+		// 	},
+		// });
+		// console.log(
+		// 	await prisma.customer.findUnique({
+		// 		where: { email: 'a' },
+		// 		include: { Address: true },
+		// 	})
+		// );
+	})
+	.catch(() => {
+		app.get('/*', (req: Request, res: Response) => {
+			return res
+				.status(500)
+				.send({ error: true, message: 'Internal server error' });
+		});
+	})
+	.finally(async () => {
+		await prisma.$disconnect();
+	});
 
 export default app;
