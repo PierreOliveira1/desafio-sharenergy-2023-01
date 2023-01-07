7abfc1e99c131a3589d422e86aab91e576ff8d70
Pierre Oliveira <pierre.torres445@gmail.com>
2023-01-07 00:21:22 -0300

    feat: add config and envs

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,50 +11,6 @@ prisma
 		app.use(bodyParser.json());
 		app.use(bodyParser.urlencoded({ extended: false }));
 		app.use('/', Routes);
-		// await prisma.customer.create({
-		// 	data: {
-		// 		email: 'b',
-		// 		cpf: 'b',
-		// 		full_name: 'c',
-		// 		phone_number: 'b',
-		// 		Address: {
-		// 			create: {
-		// 				city: 'b',
-		// 				complement: 'b',
-		// 				country: 'b',
-		// 				district: 'b',
-		// 				number: 1,
-		// 				state: 'b',
-		// 				street: 'b',
-		// 				zip_code: 'b',
-		// 			},
-		// 		},
-		// 	},
-		// });
-
-		// await prisma.customer.update({
-		// 	where: { email: 'a' },
-		// 	data: {
-		// 		Address: {
-		// 			create: {
-		// 				city: 'c',
-		// 				complement: 'c',
-		// 				country: 'c',
-		// 				district: 'c',
-		// 				number: 1,
-		// 				state: 'c',
-		// 				street: 'c',
-		// 				zip_code: 'c',
-		// 			},
-		// 		},
-		// 	},
-		// });
-		// console.log(
-		// 	await prisma.customer.findUnique({
-		// 		where: { email: 'a' },
-		// 		include: { Address: true },
-		// 	})
-		// );
 	})
 	.catch(() => {
 		app.get('/*', (req: Request, res: Response) => {
