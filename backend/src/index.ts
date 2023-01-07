49ba32508c514148cfcad5811eb8e381adf59491
Pierre Oliveira <pierre.torres445@gmail.com>
2023-01-07 12:33:31 -0300

    feat: add  env to port server - backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,9 @@
 import { createServer } from 'node:http';
 import app from './app';
+import { SERVER_PORT } from './config/envs';
 
 const Server = createServer(app);
 
-Server.listen(3333, () => {
+Server.listen(SERVER_PORT, () => {
 	console.log('Is running!');
 });
