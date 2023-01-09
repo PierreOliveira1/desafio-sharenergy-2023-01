44b0698c684f06c624a989fb23b3b111191dfa7f
Pierre Oliveira <pierre.torres445@gmail.com>
2023-01-08 22:33:32 -0300

    feat: add cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,26 +1,14 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import Routes from './routes';
-import prisma from './database';
+import { corsConfig } from '@config/corsConfig';
 
 const app = express();
 
-prisma
-	.$connect()
-	.then(async () => {
-		app.use(bodyParser.json());
-		app.use(bodyParser.urlencoded({ extended: false }));
-		app.use('/', Routes);
-	})
-	.catch(() => {
-		app.get('/*', (req: Request, res: Response) => {
-			return res
-				.status(500)
-				.send({ error: true, message: 'Internal server error' });
-		});
-	})
-	.finally(async () => {
-		await prisma.$disconnect();
-	});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors(corsConfig));
+app.use('/', Routes);
 
 export default app;
