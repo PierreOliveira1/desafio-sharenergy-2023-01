fc1f64b3a8c14675ed526f3d2b12a217c7323da1
Pierre Oliveira <pierre.torres445@gmail.com>
2023-01-08 22:39:35 -0300

    feat: better token verification and export adm id - backend

--- backend/src/middlewares/ensureAuthenticated/index.ts
+++ backend/src/middlewares/ensureAuthenticated/index.ts
@@ -1,18 +1,31 @@
-import { JWT_SECRET } from '@src/config/envs';
+import { JWT_SECRET } from '@config/envs';
+import { DecodedToken } from '@dtos/decodedToken';
 import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 
-function ensureAuthenticated(req: Request, res: Response, next: NextFunction) {
+async function ensureAuthenticated(
+	req: Request,
+	res: Response,
+	next: NextFunction
+) {
 	const authToken = req.headers.authorization;
 
 	if (!authToken) {
 		return res.status(401).send({ error: true, message: 'Token is missing' });
 	}
 
-	const [, token] = authToken.split(' ');
+	const [bearer, token] = authToken.split(' ');
+
+	if (!/Bearer/g.test(bearer)) {
+		return res
+			.status(401)
+			.send({ error: true, message: 'Badly formatted token' });
+	}
 
 	try {
-		verify(token, JWT_SECRET);
+		const decoded = verify(token, JWT_SECRET) as DecodedToken;
+
+		res.locals.id = decoded.id;
 
 		return next();
 	} catch (error) {
