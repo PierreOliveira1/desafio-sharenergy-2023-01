245449a86f30cea07e998d775a7116b3962df539
Pierre Oliveira <pierre.torres445@gmail.com>
2023-01-08 22:36:04 -0300

    feat: get and exported env cors origins

--- backend/src/config/envs/index.ts
+++ backend/src/config/envs/index.ts
@@ -2,5 +2,6 @@ import { config } from 'dotenv';
 
 config();
 
-export const JWT_SECRET = process.env.JWT_SECRET as string;
+export const JWT_SECRET = `${process.env.JWT_SECRET}`;
 export const SERVER_PORT = Number(process.env.SERVER_PORT);
+export const CORS_ORIGINS = `${process.env.CORS_ORIGINS}`.split(';');
