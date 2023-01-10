92c3d90109fb1a5d5e00bf92fd95f833d50b6981
Pierre Oliveira <pierre.torres445@gmail.com>
2023-01-10 18:38:28 -0300

    feat: configured theme interface - frontend-web

--- frontend-web/src/styled.d.ts
+++ frontend-web/src/styled.d.ts
@@ -3,5 +3,10 @@ import 'styled-components';
 declare module 'styled-components' {
   export interface DefaultTheme {
     boxShadow: string;
+		color: {
+			primary: string;
+			secondary: string;
+			tertiary: string;
+		}
 	}
 }
