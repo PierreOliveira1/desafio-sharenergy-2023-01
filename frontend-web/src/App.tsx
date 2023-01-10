3404171d33087974c2c3260e118600e9f12e5fb2
Pierre Oliveira <pierre.torres445@gmail.com>
2023-01-10 18:38:05 -0300

    feat: add query client of the react query - frontend-web

--- frontend-web/src/App.tsx
+++ frontend-web/src/App.tsx
@@ -1,13 +1,18 @@
 import { ThemeProvider } from 'styled-components';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { theme } from '@styles/theme';
 import Routes from './routes';
 import GlobalStyles from './styles/GlobalStyles';
 
+const queryClient = new QueryClient();
+
 function App() {
 	return (
 		<ThemeProvider theme={theme}>
-			<GlobalStyles />
-			<Routes />
+			<QueryClientProvider client={queryClient}>
+				<GlobalStyles />
+				<Routes />
+			</QueryClientProvider>
 		</ThemeProvider>
 	);
 }
