3bdddc8c045f65803cdda49aa2ebee6d0b54d585
Pierre Oliveira <pierre.torres445@gmail.com>
2023-01-10 18:41:06 -0300

    feat: created page of login - frontend-web

--- frontend-web/src/modules/auth/pages/Login/index.tsx
+++ frontend-web/src/modules/auth/pages/Login/index.tsx
@@ -1,16 +1,39 @@
+import { Button } from '@components/ui/Button';
+import { Checkbox } from '@components/ui/Checkbox';
 import { Input } from '@components/ui/Input';
+import { theme } from '@styles/theme';
 import { useForm, SubmitHandler } from 'react-hook-form';
+import { useMutation } from 'react-query';
+import { redirect } from 'react-router-dom';
+import { fetchApi } from '../../../../lib/fetchApi';
 import * as Styles from './styles';
 
-type Inputs ={
-	teste: string;
+type Inputs = {
+	username: string;
+	password: string;
+	rememberMe: string;
 }
 
 function Login() {
-	const { register, handleSubmit } = useForm();
+	const { register, handleSubmit } = useForm<Inputs>();
 
-	const onSubmit: SubmitHandler<Inputs> = (data) => {
-		console.log(data);
+	const mutation = useMutation(({ username, password }: Inputs) => {
+		return fetchApi('/auth/signin', {
+			method: 'POST',
+			body: JSON.stringify({
+				username,
+				password
+			})
+		});
+	});
+
+	const onSubmit: SubmitHandler<Inputs> = async ({ username, password, rememberMe }) => {
+		console.log(rememberMe);
+		mutation.mutate({ username, password, rememberMe }, {
+			onSuccess: () => {
+				redirect('/home');
+			}
+		});
 	};
 
 	return(
@@ -20,9 +43,28 @@ function Login() {
 					src="https://www.sharenergy.com.br/wp-content/uploads/2022/12/logo_color.png"
 					alt="Logo"
 				/>
-				<form>
-					<input type='type' defaultValue="a" placeholder='Teste' {...register('teste')} />
-				</form>
+				<Styles.Form onSubmit={handleSubmit(onSubmit)}>
+					<Input
+						width='80%'
+						type="text"
+						placeholder='Usuário...'
+						{...register('username')}
+					/>
+					<Styles.BoxInputCheckbox>
+						<Input
+							width='80%'
+							type="text"
+							placeholder='Senha...'
+							isPassword
+							margin='0 0 10px 0'
+							{...register('password')}
+						/>
+						<Styles.BoxCheckbox>
+							<Checkbox color={theme.color.tertiary} {...register('rememberMe')} label='Manter-me conectado' />
+						</Styles.BoxCheckbox>
+					</Styles.BoxInputCheckbox>
+					<Button isSubmit value="Entrar" />
+				</Styles.Form>
 			</Styles.Content>
 		</Styles.Container>
 	);
--- frontend-web/src/modules/auth/pages/Login/styles.ts
+++ frontend-web/src/modules/auth/pages/Login/styles.ts
@@ -7,6 +7,8 @@ export const Container = styled.div`
 	display: flex;
 	justify-content: center;
 	align-items: center;
+	background-image: url('https://www.sharenergy.com.br/wp-content/uploads/2017/12/Setor-Solar.jpg');
+	background-size: cover;
 `;
 
 export const Content = styled.div`
@@ -19,18 +21,52 @@ export const Content = styled.div`
 	background-color: red;
 	padding: 2rem;
 	border-radius: 1rem;
+	background: rgba( 255, 255, 255, 0.35 );
+  box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+  backdrop-filter: blur( 13.5px );
 
 	@media screen and (max-width: 1024px) {
 		width: 70%;
 	}
 
 	@media screen and  (max-width: 768px) {
-		width: 100%;
-		height: 100vh;
-		border-radius: 0;
+		width: 85%;
+		height: 85%;
 	}
 `;
 
 export const Logo = styled.img`
 	width: 50%;
+
+	@media screen and (max-width: 1400px) {
+		width: 60%;
+	}
+
+	@media screen and (max-width: 1024px) {
+		width: 65%;
+	}
+
+	@media screen and  (max-width: 768px) {
+		width: 70%;
+	}
+`;
+
+export const Form = styled.form`
+	width: 100%;
+	display: flex;
+	align-items	: center;
+	flex-direction: column;
+	margin-top: 3rem;
+`;
+
+export const BoxInputCheckbox = styled.div`
+	width: 100%;
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+	margin: 2rem 0;
+`;
+
+export const BoxCheckbox = styled.div`
+	width: 80%;
 `;
