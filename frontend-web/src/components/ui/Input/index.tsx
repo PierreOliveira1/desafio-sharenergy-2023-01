ece03fbddcce7462aa5257cc89f89f749ef00b1f
Pierre Oliveira <pierre.torres445@gmail.com>
2023-01-10 18:39:59 -0300

    feat: created component input - frontend-web

--- frontend-web/src/components/ui/Input/index.tsx
+++ frontend-web/src/components/ui/Input/index.tsx
@@ -1,18 +1,42 @@
-import { ChangeEvent, HTMLInputTypeAttribute } from 'react';
+import { forwardRef, InputHTMLAttributes, MouseEvent, Ref, useState } from 'react';
 import * as Styles from './styles';
+import { EyeOpenIcon, EyeClosedIcon } from '@radix-ui/react-icons';
 
-interface Props {
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
 	width?: string;
 	height?: string;
-	placeholder?: string;
-	type: HTMLInputTypeAttribute;
-	onChange?: (value: ChangeEvent<HTMLInputElement>) => void;
+	margin?: string;
+	isPassword?: boolean;
+	type?: string;
 }
 
-function Input(props: Props) {
-	const { width, height, onChange, placeholder, type } = props;
+function InputComponent(props: Props, ref: Ref<HTMLInputElement>) {
+	const [viewPassword, setViewPassword] = useState(true);
+	const { width, height, margin, isPassword, type, ...rest } = props;
 
-	return <Styles.Input width={width} height={height} type={type} placeholder={placeholder} onChange={onChange} />;
+	function handleViewPassword(event: MouseEvent<HTMLElement>) {
+		event.preventDefault();
+		setViewPassword(state => !state);
+	}
+
+	const typePassword = isPassword ? viewPassword ? 'password' : 'text' : type;
+
+	return <Styles.InputContainer width={width} height={height} margin={margin} >
+		<Styles.Input
+			ref={ref}
+			type={typePassword}
+			{...rest}
+		/>
+		{isPassword && (
+			<Styles.Icon onClick={handleViewPassword}>
+				{
+					viewPassword ? <EyeClosedIcon /> : <EyeOpenIcon />
+				}
+			</Styles.Icon>
+		)}
+	</Styles.InputContainer>;
 }
+
+const Input = forwardRef(InputComponent);
 
 export { Input };
--- frontend-web/src/components/ui/Input/styles.ts
+++ frontend-web/src/components/ui/Input/styles.ts
@@ -1,12 +1,35 @@
 import styled from 'styled-components';
 
-export const Input = styled.input<{
+export interface StylesInput {
 	width?: string;
 	height?: string;
-}>`
+	margin?: string;
+}
+
+export const InputContainer = styled.div<StylesInput>`
 	width: ${({ width }) => width || '100%'};
 	height: ${({ height }) => height || '40px'};
-	padding: 10px;
-	border-radius: 10px;
 	box-shadow: ${({ theme }) => theme.boxShadow};
+	margin: ${({ margin }) => margin || '0'};
+	border-radius: 10px;
+	display: flex;
+	background-color: ${({ theme }) => theme.color.tertiary};
+`;
+
+export const Input = styled.input`
+	width: 100%;
+	height: 100%;
+	padding: 0 20px;
+	border-radius: 10px;
+`;
+
+export const Icon = styled.button`
+	width: fit-content;
+	height: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	background: none;
+	padding: 10px;
+	cursor: pointer;
 `;
