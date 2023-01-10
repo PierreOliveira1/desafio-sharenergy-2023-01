8df2b88557903daa3585be1e43297bdc911ce722
Pierre Oliveira <pierre.torres445@gmail.com>
2023-01-10 18:38:58 -0300

    feat: created component to loading - frontend-web

---
+++ frontend-web/src/components/common/Loading/styles.ts
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+	width: 100%;
+	height: 100vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+
+export const OnLoading = styled.div`
+	display: flex;
+
+	& div {
+		width: 50px;
+		height: 50px;
+		background: ${({ theme }) => theme.color.primary};
+		border-radius: 50%;
+		margin-left: 15px;
+
+		animation: move 1s ease-in-out infinite alternate;
+	}
+
+	& div:nth-child(1) {
+		animation-delay: -0.4s;
+	}
+
+	& div:nth-child(2) {
+		animation-delay: -0.2s;
+	}
+
+	@keyframes move {
+		from {
+			transform: translateY(-100%);
+		}
+		to {
+			transform: translateY(100%);
+		}
+	}
+`;
