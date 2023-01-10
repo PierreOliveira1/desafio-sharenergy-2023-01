fc078c3af49566cbc801b2aee138336d92bea9ae
Pierre Oliveira <pierre.torres445@gmail.com>
2023-01-10 12:03:28 -0300

    feat: configured font family global - frontend-web

--- frontend-web/src/styles/GlobalStyles.ts
+++ frontend-web/src/styles/GlobalStyles.ts
@@ -1,10 +1,15 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
+	@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
 	* {
 		margin: 0;
 		padding: 0;
+		border: 0;
+		outline: 0;
 		box-sizing: border-box;
+		font-family: 'Roboto', sans-serif;
 	}
 `;
 
