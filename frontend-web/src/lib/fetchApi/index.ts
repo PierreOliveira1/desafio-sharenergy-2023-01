fe897040e9d2cdaf5f2f750d39d9cac3db90955f
Pierre Oliveira <pierre.torres445@gmail.com>
2023-01-10 18:40:44 -0300

    feat: configured function api - frontend-web

---
+++ frontend-web/src/lib/fetchApi/index.ts
@@ -0,0 +1,35 @@
+import { API_URL, TOKEN_KEY } from '../../config/envs';
+
+class ApiError extends Error {
+	code: number;
+	constructor(
+		code: number,
+		message?: string | undefined,
+		options?: ErrorOptions | undefined,
+	) {
+		super(message, options);
+		this.code = code;
+	}
+}
+
+async function fetchApi(url: string, init?: RequestInit | undefined): Promise<Response> {
+	const token = localStorage.getItem(TOKEN_KEY);
+
+	const response = await fetch(API_URL + url, {
+		...init,
+		headers: {
+			Authorization: token ? `Bearer ${token}` : '',
+			...init?.headers,
+		}
+	});
+
+	if(response.status >= 200 && response.status < 300) {
+		return response;
+	} else {
+		const { error } = (await response.json()) as { error: string };
+
+		throw new ApiError(response.status, `${error}`);
+	}
+}
+
+export { fetchApi };
