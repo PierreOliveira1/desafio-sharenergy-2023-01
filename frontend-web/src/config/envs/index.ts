f876725274c2b13762bd84841bb844979943faf1
Pierre Oliveira <pierre.torres445@gmail.com>
2023-01-11 00:30:48 -0300

    feat: add env to refresh token key - frontend-web

--- frontend-web/src/config/envs/index.ts
+++ frontend-web/src/config/envs/index.ts
@@ -1,2 +1,3 @@
 export const API_URL = `${import.meta.env.VITE_API_URL}`;
 export const TOKEN_KEY = `${import.meta.env.VITE_TOKEN_KEY}`;
+export const REFRESH_TOKEN_KEY = `${import.meta.env.VITE_REFRESH_TOKEN_KEY}`;
