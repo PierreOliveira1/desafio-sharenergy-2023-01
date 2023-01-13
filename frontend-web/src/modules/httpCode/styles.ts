b309db42e7a3dd2864f326b51c550aab69f08279
Pierre Oliveira <pierre.torres445@gmail.com>
2023-01-13 18:48:58 -0300

    feat: create component to button http code - frontend-web

---
+++ frontend-web/src/modules/httpCode/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+	width: 100%;
+	height: 100%;
+	padding-top: 60px;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	flex-direction: column;
+`;
+
+export const Image = styled.div``;
