059b68ef3a21f48ffe0cb82325edba9a3e3cfae3
Pierre Oliveira <pierre.torres445@gmail.com>
2023-01-14 13:48:48 -0300

    feat: stop loading initial route - frontend-web

--- frontend-web/src/modules/httpCode/index.tsx
+++ frontend-web/src/modules/httpCode/index.tsx
@@ -1,7 +1,15 @@
+import { useLoading } from '@stores/isLoading';
+import { useEffect } from 'react';
 import { ListSessions } from './components/common/ListSessions';
 import * as Styles from './styles';
 
 function HttpCode() {
+	const { isLoading, setIsLoading } = useLoading();
+
+	useEffect(() => {
+		if(isLoading) setIsLoading(false);
+	}, []);
+
 	return (
 		<Styles.Container>
 			<Styles.Image />
