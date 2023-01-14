afe8322d6d0f86da91830713c14915e127b14e4e
Pierre Oliveira <pierre.torres445@gmail.com>
2023-01-14 00:07:14 -0300

    refactor: change colors theme - frontend-web

--- frontend-web/src/styles/theme.ts
+++ frontend-web/src/styles/theme.ts
@@ -2,9 +2,10 @@ import { DefaultTheme } from 'styled-components';
 
 export const theme: DefaultTheme = {
 	boxShadow: '5px 5px 20px rgba(36, 36, 36, 0.4)',
+	border: '1px solid #3434',
 	color: {
 		primary: '#00A2A2',
-		secondary: '#000000',
-		tertiary: '#FFFFFF'
+		secondary: '#111111',
+		tertiary: '#F1F1F1'
 	}
 };
