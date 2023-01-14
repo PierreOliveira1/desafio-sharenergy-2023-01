c934de68f32cdce523996757ac37b829f0e658e8
Pierre Oliveira <pierre.torres445@gmail.com>
2023-01-13 23:56:28 -0300

    feat: configured query client - frontend-web

--- frontend-web/src/App.tsx
+++ frontend-web/src/App.tsx
@@ -3,8 +3,9 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import { theme } from '@styles/theme';
 import Routes from './routes';
 import GlobalStyles from './styles/GlobalStyles';
+import { configQueryClient } from './config/configQueryClient';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient(configQueryClient);
 
 function App() {
 	return (
