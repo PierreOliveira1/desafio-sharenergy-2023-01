c5b7511cc4c10fc4ab7b3a364f0245cd87b2b290
Pierre Oliveira <pierre.torres445@gmail.com>
2023-01-14 13:47:46 -0300

    feat: create active loading in component - frontend-web

--- frontend-web/src/components/common/Loading/index.tsx
+++ frontend-web/src/components/common/Loading/index.tsx
@@ -1,8 +1,23 @@
 import * as Styles from './styles';
+import { useLoading } from '@stores/isLoading';
+import { Variants } from 'framer-motion';
 
 function Loading() {
+	const { isLoading } = useLoading();
+
+	const variantsAnimateLoadingView: Variants = {
+		open: {
+			display: 'flex',
+		},
+		closed: {
+			display: 'none',
+		}
+	};
+
+	const animate = !isLoading ? 'closed' : 'open';
+
 	return (
-		<Styles.Container>
+		<Styles.Container variants={variantsAnimateLoadingView} animate={animate}>
 			<Styles.OnLoading>
 				<div></div>
 				<div></div>
