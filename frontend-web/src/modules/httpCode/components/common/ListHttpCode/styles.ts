9067da2ca3fe6a6dc972e29ac09de03fe3283aa6
Pierre Oliveira <pierre.torres445@gmail.com>
2023-01-15 17:12:42 -0300

    feat: add scroll list http codes - frontend-web

--- frontend-web/src/modules/httpCode/components/common/ListHttpCode/styles.ts
+++ frontend-web/src/modules/httpCode/components/common/ListHttpCode/styles.ts
@@ -4,8 +4,21 @@ export const Content = styled.div`
 	display: flex;
 	flex-wrap: wrap;
 	width: 100%;
-	height: fit-content;
+	height: auto;
+	max-height: 200px;
 	padding: 2% 0;
+
+	&::-webkit-scrollbar {
+		width: 10px;
+		background-color: #CCCCCC;
+		border-radius: 10px;
+	}
+
+	&::-webkit-scrollbar-thumb {
+		width: 10px;
+		background-color: #777777;
+		border-radius: 10px;
+	}
 `;
 
 export const BoxHttpCode = styled.div`
