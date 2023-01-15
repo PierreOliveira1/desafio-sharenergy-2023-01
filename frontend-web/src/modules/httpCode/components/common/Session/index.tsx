f290b2afc5d15e141173363b11e5e53e40442f89
Pierre Oliveira <pierre.torres445@gmail.com>
2023-01-15 17:19:46 -0300

    feat: final session - frontend-web

--- frontend-web/src/modules/httpCode/components/common/Session/index.tsx
+++ frontend-web/src/modules/httpCode/components/common/Session/index.tsx
@@ -1,15 +1,16 @@
 import { ChevronDownIcon } from '@radix-ui/react-icons';
-import { motion, Variants } from 'framer-motion';
-import { useState } from 'react';
+import { motion, Variants, AnimatePresence } from 'framer-motion';
+import { useSession } from '../ListSessions/stores/useSession';
 import * as Styles from './styles';
 
 interface Props {
+	id: number;
 	title: string;
 	children: JSX.Element;
 }
 
-function Session({ children, title }: Props) {
-	const [isOpen, setIsOpen] = useState(false);
+function Session({ children, title, id }: Props) {
+	const { session, setSession } = useSession();
 
 	const variantsIcon: Variants = {
 		open: {
@@ -20,22 +21,48 @@ function Session({ children, title }: Props) {
 		}
 	};
 
+	const variantsBoxHttpCodes: Variants = {
+		open: {
+			height: 'fit-content',
+			display: 'flex',
+			opacity: 1,
+		},
+		closed: {
+			height: 0,
+			display: 'none',
+			opacity: 0,
+		}
+	};
+
 	return (
-		<>
-			<Styles.Content onClick={() => setIsOpen(state => !state)} role="button">
+		<Styles.Container>
+			<Styles.Content
+				onClick={() => setSession(session === id ? 0 : id)}
+				role="button"
+			>
 				<Styles.BoxName>
 					<Styles.Title>{title}</Styles.Title>
 				</Styles.BoxName>
 				<Styles.BoxIcon>
-					<motion.div variants={variantsIcon} animate={isOpen ? 'open' : 'closed'}>
+					<motion.div
+						variants={variantsIcon}
+						animate={session === id ? 'open' : 'closed'}
+						transition={{ duration: session === id && .5 }}
+					>
 						<ChevronDownIcon width="1.5rem" height="1.5rem" />
 					</motion.div>
 				</Styles.BoxIcon>
 			</Styles.Content>
-			<Styles.BoxHttpCodes>
-				{children}
-			</Styles.BoxHttpCodes>
-		</>
+			<AnimatePresence>
+				<Styles.BoxHttpCodes
+					variants={variantsBoxHttpCodes}
+					animate={session === id ? 'open' : 'closed'}
+					transition={{ duration: .5 }}
+				>
+					{children}
+				</Styles.BoxHttpCodes>
+			</AnimatePresence>
+		</Styles.Container>
 	);
 }
 
