66a594a9bf03ff86d8512e1d0858df60ee143ff8
Pierre Oliveira <pierre.torres445@gmail.com>
2023-01-15 12:29:48 -0300

    feat: configured search user client - frontend-web

--- frontend-web/src/modules/users/components/common/ListUsers/index.tsx
+++ frontend-web/src/modules/users/components/common/ListUsers/index.tsx
@@ -1,4 +1,5 @@
 import { User } from '@modules/users/dtos/user';
+import { useInfoQuery } from '@modules/users/stores/useInfoQuery';
 import { BoxUser } from '../BoxUser';
 import * as Styles from './styles';
 
@@ -7,13 +8,27 @@ interface Props {
 }
 
 function ListUsers({ users }: Props) {
+	const { search } = useInfoQuery();
+
 	return (
 		<Styles.Content>
-			{users.map((user, index) => (
-				<Styles.ContentUser key={index}>
-					<BoxUser {...user} />
-				</Styles.ContentUser>
-			))}
+			{users.map((user, index) => {
+				if(search) {
+					const isSearch = `${user.name.first} ${user.name.last}`.toLowerCase().includes(search.toLowerCase());
+					if(isSearch)
+						return (
+							<Styles.ContentUser key={index}>
+								<BoxUser {...user} />
+							</Styles.ContentUser>
+						);
+				} else {
+					return (
+						<Styles.ContentUser key={index}>
+							<BoxUser {...user} />
+						</Styles.ContentUser>
+					);
+				}
+			})}
 		</Styles.Content>
 	);
 }
