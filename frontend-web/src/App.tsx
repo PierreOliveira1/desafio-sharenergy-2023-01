795c876be735e962ed83bbdcfa6111a4dba3936a
Pierre Oliveira <pierre.torres445@gmail.com>
2023-01-15 12:26:29 -0300

    feat: configured loading - frontend-web

--- frontend-web/src/App.tsx
+++ frontend-web/src/App.tsx
@@ -5,9 +5,17 @@ import Routes from './routes';
 import GlobalStyles from '@styles/GlobalStyles';
 import { queryClient } from '@config/queryClient';
 import { Loading } from '@components/common/Loading';
+import { useLoading } from '@stores/isLoading';
+import { useEffect } from 'react';
 
 
 function App() {
+	const { setIsLoading } = useLoading();
+
+	useEffect(() => {
+		setIsLoading(true);
+	}, []);
+
 	return (
 		<ThemeProvider theme={theme}>
 			<Loading />
