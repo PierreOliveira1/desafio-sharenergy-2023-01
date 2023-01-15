e8e00040d18ba9c88296ddab5e4522d175608aaf
Pierre Oliveira <pierre.torres445@gmail.com>
2023-01-15 12:29:24 -0300

    feat: final page of random users - frontend-web

--- frontend-web/src/modules/users/index.tsx
+++ frontend-web/src/modules/users/index.tsx
@@ -1,62 +1,76 @@
 import { Input } from '@components/ui/Input';
 import { useLoading } from '@stores/isLoading';
-import { useEffect } from 'react';
-import { useCallback } from 'react';
-import { ChangeEvent } from 'react';
+import { useEffect, useRef } from 'react';
 import { useQuery } from 'react-query';
-import { useSearchParams } from 'react-router-dom';
 import { ListUsers } from './components/common/ListUsers';
 import { Data } from './dtos/data';
 import { User } from './dtos/user';
 import * as Styles from './styles';
+import { MagnifyingGlassIcon } from '@radix-ui/react-icons';
+import { Pagination } from '@components/common/Pagination';
+import { useInfoQuery } from './stores/useInfoQuery';
 
 function Users() {
-	const [searchParams, setSearchParams] = useSearchParams();
 	const { setIsLoading } = useLoading();
-	const users = useQuery<Data<User>, Error>('users', async () => {
-		const users = await fetch('https://randomuser.me/api/?inc=picture,name,email,login,dob&results=100');
+	const { page, setPage, setSearch, seed, setSeed } = useInfoQuery();
+	const searchRef = useRef<HTMLInputElement>(null);
+	const users = useQuery<Data<User>, Error>(['users', { seed, page }], async () => {
+		const query = `https://randomuser.me/api/?inc=picture,name,email,login,dob&nat=br&results=100&page=${page}&seed=${seed}`;
 
-		return await users.json();
+		setIsLoading(true);
+
+		const users = await fetch(query);
+
+		const response: Data<User> = await users.json();
+
+		setPage(response.info.page);
+		setSeed(response.info.seed);
+
+		return response;
 	});
 
-	function handleSearch(event: ChangeEvent<HTMLInputElement>) {
-		setTimeout(() => {
-			const search = event.target.value;
-			if (search) {
-				searchParams.set('search', search);
-			} else {
-				searchParams.delete('search');
-			}
-			setSearchParams(searchParams);
-		}, 700);
+	function handleSearch(search: string) {
+		if (search) {
+			setSearch(search);
+		} else {
+			setSearch('');
+		}
 	}
-
-	const changeSearch = useCallback((event: ChangeEvent<HTMLInputElement>) => {
-		handleSearch(event);
-	}, []);
 
 	useEffect(() => {
 		if(!users.isLoading)
-			setIsLoading(false);
+			setTimeout(() => {
+				setIsLoading(false);
+			}, 1000);
 	}, [users.isLoading, users.isFetching]);
 
-	if(users.isSuccess) {
-		return (
-			<Styles.Container>
-				<Styles.BoxSearch>
-					<Input
-						width='100%'
-						placeholder='Busque um usuário...'
-					/>
-				</Styles.BoxSearch>
-				<ListUsers
-					users={users.data.results}
+	return (
+		<Styles.Container>
+			<Styles.BoxSearch>
+				<Input
+					ref={searchRef}
+					width='100%'
+					placeholder='Busque um usuário...'
+					onKeyDown={event => {
+						if(event.key === 'Enter') {
+							event.preventDefault();
+							handleSearch(searchRef.current?.value ?? '');
+						}
+					}}
+					icon={<MagnifyingGlassIcon width="1.2rem" height="1.2rem" />}
+					handleIconClick={event => {
+						event.preventDefault();
+						handleSearch(searchRef.current?.value ??'');
+					}}
 				/>
-			</Styles.Container>
-		);
-	}
+			</Styles.BoxSearch>
+			{users.isSuccess && <ListUsers
+				users={users.data.results}
+			/>}
+			<Pagination page={Number(page)} setPage={setPage} />
+		</Styles.Container>
+	);
 
-	return <></>;
 }
 
 export default Users;
