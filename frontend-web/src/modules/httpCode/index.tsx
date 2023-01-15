e08f8c921b398a0a2298087e68e06a3271d33d32
Pierre Oliveira <pierre.torres445@gmail.com>
2023-01-15 17:11:40 -0300

    feat: add image with store http code selected - frontend-web

--- frontend-web/src/modules/httpCode/index.tsx
+++ frontend-web/src/modules/httpCode/index.tsx
@@ -1,10 +1,12 @@
 import { useLoading } from '@stores/isLoading';
 import { useEffect } from 'react';
 import { ListSessions } from './components/common/ListSessions';
+import { useHttpCode } from './store/useHttpCode';
 import * as Styles from './styles';
 
 function HttpCode() {
 	const { isLoading, setIsLoading } = useLoading();
+	const { httpCode } = useHttpCode();
 
 	useEffect(() => {
 		if(isLoading) setIsLoading(false);
@@ -12,7 +14,7 @@ function HttpCode() {
 
 	return (
 		<Styles.Container>
-			<Styles.Image />
+			<Styles.Image src={`https://http.cat/${httpCode}`} />
 			<ListSessions />
 		</Styles.Container>
 	);
